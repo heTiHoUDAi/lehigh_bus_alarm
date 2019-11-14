689e31de3e6dea4df560e866e7a1f9eae1676328
zisheng wang <zs.wang.prc@gmail.com>
2019-11-13 20:24:45 -0800

    Initial Commit

---
+++ bus_alarm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bus_alarm
+//
+//  Created by Zisheng Wang on 11/13/19.
+//  Copyright © 2019 Zisheng Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
